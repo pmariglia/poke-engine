d5d6d81e88aea59892ff31d4809f11cd4dd6a61e
pmariglia <mariglia4423@gmail.com>
2024-02-10 15:30:12 -0500

    some tests

--- tests/test_battle_mechanics.rs
+++ tests/test_battle_mechanics.rs
@@ -1,45 +1,43 @@
 use poke_engine::generate_instructions::generate_instructions_from_move_pair;
-use poke_engine::instruction::{BoostInstruction, DamageInstruction, Instruction, StateInstructions};
-use poke_engine::state::{PokemonBoostableStat, SideReference, State};
+use poke_engine::instruction::{ApplyVolatileStatusInstruction, BoostInstruction, DamageInstruction, Instruction, RemoveVolatileStatusInstruction, StateInstructions};
+use poke_engine::state::{PokemonBoostableStat, PokemonVolatileStatus, SideReference, State};
 
 #[test]
 fn test_basic_move_pair_instruction_generation() {
     let mut state = State::default();
-    state.side_one.get_active().speed = 100;
-    state.side_two.get_active().speed = 50;
-    let side_one_move = String::from("tackle");
-    let side_two_move = String::from("tackle");
 
-    let vec_of_instructions =
-        generate_instructions_from_move_pair(&mut state, side_one_move, side_two_move);
+    let vec_of_instructions = generate_instructions_from_move_pair(
+        &mut state,
+        String::from("tackle"),
+        String::from("tackle")
+    );
 
     let expected_instructions = vec![StateInstructions {
         percentage: 100.0,
         instruction_list: vec![
             Instruction::Damage(DamageInstruction {
-                side_ref: SideReference::SideTwo,
+                side_ref: SideReference::SideOne,
                 damage_amount: 48,
             }),
             Instruction::Damage(DamageInstruction {
-                side_ref: SideReference::SideOne,
+                side_ref: SideReference::SideTwo,
                 damage_amount: 48,
             }),
         ],
     }];
-
     assert_eq!(expected_instructions, vec_of_instructions)
 }
 
 #[test]
 fn test_move_pair_instruction_generation_where_first_move_branches() {
     let mut state = State::default();
-    state.side_one.get_active().speed = 100;
-    state.side_two.get_active().speed = 50;
-    let side_one_move = String::from("playrough");
-    let side_two_move = String::from("tackle");
+    state.side_one.get_active().speed = 101;
 
-    let vec_of_instructions =
-        generate_instructions_from_move_pair(&mut state, side_one_move, side_two_move);
+    let vec_of_instructions = generate_instructions_from_move_pair(
+        &mut state,
+        String::from("playrough"),
+        String::from("tackle")
+    );
 
     let expected_instructions = vec![
         StateInstructions {
@@ -82,7 +80,6 @@ fn test_move_pair_instruction_generation_where_first_move_branches() {
             ],
         },
     ];
-
     assert_eq!(expected_instructions, vec_of_instructions)
 }
 
@@ -90,12 +87,12 @@ fn test_move_pair_instruction_generation_where_first_move_branches() {
 fn test_move_pair_instruction_generation_where_second_move_branches() {
     let mut state = State::default();
     state.side_one.get_active().speed = 50;
-    state.side_two.get_active().speed = 100;
-    let side_one_move = String::from("playrough");
-    let side_two_move = String::from("tackle");
 
-    let vec_of_instructions =
-        generate_instructions_from_move_pair(&mut state, side_one_move, side_two_move);
+    let vec_of_instructions = generate_instructions_from_move_pair(
+        &mut state,
+        String::from("playrough"),
+        String::from("tackle")
+    );
 
     let expected_instructions = vec![
         StateInstructions {
@@ -137,6 +134,106 @@ fn test_move_pair_instruction_generation_where_second_move_branches() {
             ],
         },
     ];
+    assert_eq!(expected_instructions, vec_of_instructions)
+}
 
+#[test]
+fn test_basic_flinching_functionality() {
+    let mut state = State::default();
+    state.side_one.get_active().speed = 150; // faster than side two
+
+    let vec_of_instructions = generate_instructions_from_move_pair(
+        &mut state,
+        String::from("ironhead"),
+        String::from("tackle")
+    );
+
+    let expected_instructions = vec![
+        StateInstructions {
+            percentage: 30.0000019,
+            instruction_list: vec![
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideTwo,
+                    damage_amount: 63,
+                }),
+                Instruction::ApplyVolatileStatus(ApplyVolatileStatusInstruction {
+                    side_ref: SideReference::SideTwo,
+                    volatile_status: PokemonVolatileStatus::Flinch,
+                }),
+                Instruction::RemoveVolatileStatus(RemoveVolatileStatusInstruction {
+                    side_ref: SideReference::SideTwo,
+                    volatile_status: PokemonVolatileStatus::Flinch,
+                }),
+            ],
+        },
+        StateInstructions {
+            percentage: 70.0,
+            instruction_list: vec![
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideTwo,
+                    damage_amount: 63,
+                }),
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideOne,
+                    damage_amount: 48,
+                }),
+            ],
+        },
+    ];
+    assert_eq!(expected_instructions, vec_of_instructions)
+}
+
+#[test]
+fn test_flinching_on_move_that_can_miss() {
+    let mut state = State::default();
+    state.side_one.get_active().speed = 150; // faster than side two
+
+    let vec_of_instructions = generate_instructions_from_move_pair(
+        &mut state,
+        String::from("airslash"),
+        String::from("tackle")
+    );
+
+    let expected_instructions = vec![
+        StateInstructions {
+            percentage: 5.000001,
+            instruction_list: vec![
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideOne,
+                    damage_amount: 48,
+                }),
+            ],
+        },
+        StateInstructions {
+            percentage: 28.5000019,
+            instruction_list: vec![
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideTwo,
+                    damage_amount: 60,
+                }),
+                Instruction::ApplyVolatileStatus(ApplyVolatileStatusInstruction {
+                    side_ref: SideReference::SideTwo,
+                    volatile_status: PokemonVolatileStatus::Flinch,
+                }),
+                Instruction::RemoveVolatileStatus(RemoveVolatileStatusInstruction {
+                    side_ref: SideReference::SideTwo,
+                    volatile_status: PokemonVolatileStatus::Flinch,
+                }),
+            ],
+        },
+        StateInstructions {
+            percentage: 66.5,
+            instruction_list: vec![
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideTwo,
+                    damage_amount: 60,
+                }),
+                Instruction::Damage(DamageInstruction {
+                    side_ref: SideReference::SideOne,
+                    damage_amount: 48,
+                }),
+            ],
+        },
+    ];
     assert_eq!(expected_instructions, vec_of_instructions)
 }
