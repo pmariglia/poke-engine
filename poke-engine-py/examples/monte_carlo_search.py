d8a1a1c3974bf962c2d3e51d11603bf6017420fc
pmariglia <mariglia4423@gmail.com>
2024-09-08 14:07:25 -0400

    Fix monte-carlo python example

--- poke-engine-py/examples/monte_carlo_search.py
+++ poke-engine-py/examples/monte_carlo_search.py
@@ -4,6 +4,6 @@ from example_state import state
 
 
 result = monte_carlo_tree_search(state, duration_ms=1000)
-print(f"Total Iterations: {result.iteration_count}")
-print([(i.move_choice, i.total_score, i.visits) for i in result.s1])
-print([(i.move_choice, i.total_score, i.visits) for i in result.s2])
+print(f"Total Iterations: {result.total_visits}")
+print([(i.move_choice, i.total_score, i.visits) for i in result.side_one])
+print([(i.move_choice, i.total_score, i.visits) for i in result.side_two])
